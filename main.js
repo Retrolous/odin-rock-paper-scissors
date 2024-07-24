c1f676e7b6703bcf3bd261828bccf1979845c3f7
retrolous <lukaspetkunas07@gmail.com>
2024-07-24 22:39:26 +0300

    Add the getHumanChoice() function
    This function will be able to gain input from the user, determining
    their move and not letting them continue until they make a valid move

--- main.js
+++ main.js
@@ -8,6 +8,27 @@ const CHOICES = ["rock", "paper", "scissors"]
 
 // Take input from user
 
+function getHumanChoice(){
+
+    let choiceMade = false;
+
+    while(!choiceMade){
+    let userChoice = prompt("What will be your move? \n(Rock, Paper or Scissors) \n").toLowerCase();
+    console.log(userChoice);
+    switch (userChoice){
+        case "scissors":
+            return "scissors";
+        case "rock":
+            return "rock";
+        case "paper":
+            return "paper";
+        default:
+            break;
+    }
+    alert("You made an invalid choice!")
+    }
+}
+
 // Generate selection
 
 function getComputerChoice(){
@@ -28,3 +49,5 @@ function randomInt(min = 0, max){
 
 console.log(randomInt(0, 2))
 console.log(getComputerChoice());
+
+console.log(getHumanChoice());
