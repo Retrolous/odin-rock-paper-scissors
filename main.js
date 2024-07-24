6c02c29801826de19be621d6013abd98e9bbcac6
retrolous <lukaspetkunas07@gmail.com>
2024-07-24 23:03:44 +0300

    Add playRound() function
    This function compares the choices by the human and computer in order to
    determine the winner and consequently increment their scores.

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 console.log("We ball");
 
 // Set score to 0, for user and computer
-let userScore = 0;
+let humanScore = 0;
 let computerScore = 0;
 let gameOver = false;
 const CHOICES = ["rock", "paper", "scissors"]
 
 // Take input from user
 
-function getHumanChoice(){
+function getHumanChoice(userChoice){
 
     let choiceMade = false;
 
     while(!choiceMade){
     let userChoice = prompt("What will be your move? \n(Rock, Paper or Scissors) \n").toLowerCase();
-    console.log(userChoice);
     switch (userChoice){
         case "scissors":
             return "scissors";
@@ -43,11 +42,54 @@ function randomInt(min = 0, max){
     return Math.floor((Math.random() * ((max + 1) - min)) + min)
 }
 // Compare input with selection
+
+function playRound(humanChoice, computerChoice){
+    
+    if(humanChoice === "scissors"){
+        if(computerChoice === "rock"){
+            console.log(`You lost! Your ${humanChoice} were beaten by ${computerChoice}`);
+            computerScore++;
+        } else if (computerChoice === "paper") {
+            console.log(`You won! Your ${humanChoice} beat ${computerChoice}`);
+            humanScore++
+        } else {
+            console.log("Draw! No-one's score increases!")
+        }
+    }
+    else if(humanChoice === "rock"){
+        if(computerChoice === "paper"){
+            console.log(`You lost! Your ${humanChoice} was beaten by ${computerChoice}`);
+            computerScore++;
+        } else if(computerChoice === "scissors") {
+            console.log(`You won! Your ${humanChoice} beat ${computerChoice}`);
+            humanScore++
+        } else {
+            console.log("Draw! No-one's score increases!")
+        }
+    }
+    else if(humanChoice === "paper"){
+        if(computerChoice === "scissors"){
+            console.log(`You lost! Your ${humanChoice} was beaten by ${computerChoice}`);
+            computerScore++;
+        } else if(computerChoice === "rock"){
+            console.log(`You won! Your ${humanChoice} beat ${computerChoice}`);
+            humanScore++
+        } else {
+            console.log("Draw! No-one's score increases!")
+        }
+    }
+}
+
 // Determine winner
 // Add score to winner
 // If score is 3 for either, game over
 
 console.log(randomInt(0, 2))
-console.log(getComputerChoice());
 
-console.log(getHumanChoice());
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+console.log(`Human selected ${humanSelection}`);
+console.log(`Computer selected ${computerSelection}`);
+playRound(humanSelection, computerSelection);
